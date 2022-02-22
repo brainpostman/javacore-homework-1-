a1ea11898e2b536d84f30b378b574229d124197a
Marat Sattarov <sattarovmk@yandex.ru>
2022-02-23 03:14:25 +0500

    Exception handling

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -7,10 +7,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> {
-        if (y == 0) throw new ArithmeticException("Деление на ноль");
-        return x / y;
-    };
+    BinaryOperator<Integer> divide = (x, y) -> x / y;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 	    Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         // деление на ноль
-        int c = calc.divide.apply(a, b);
-        calc.println.accept(c);
+        try {
+            int c = calc.divide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException ex) {
+            System.out.println("Деление на ноль (" + a + " / " + b +").");
+        }
     }
 }
